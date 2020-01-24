c3fdb6612f5c9251c79a2008c3ef206f382645de
Ephraim Amiel Yusi <ephraimyusi@gmail.com>
2020-01-24 21:34:46 +1000

    Added CSS breakpoint and form submission, and data collection

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-document.getElementById("starter-form").addEventListener('mouseup', function(e){
-  console.log(e.target)
+// Gameplay object which will manage our data and associate functions with said data
+
+let gameplay = {
+  playerNo : 0,
+  playerLife : 0,
+  playerStats: {},
+  initData : function(array){
+    this.playerNo = array[0];
+    this.playerLife = array[1];
+    this.createPlayers();
+  },
+  createPlayers: function(){
+    for (let i = 0; i < this.playerNo; i++) {
+      this.playerStats['Player ' + (i + 1)] = {"Lives" : this.playerLife};
+    }
+    alert('Functioning');
+  }
+
+}
+
+// function for submitting of start form
+
+document.getElementById("starter-form").addEventListener('click', function(e){
+  //Start of submit processing block
+  if(e.target.classList.contains("button-green")){
+    //Check if submit button
+    e.preventDefault();
+    let playerData = [];
+
+    //Grab data and add to playerData array
+    for (let i = 0; i < e.target.parentNode.elements.length - 1; i++) {
+      playerData.push(parseInt(e.target.parentNode.elements[i].value));
+    }
+
+    //Pushing data into our state manager.
+    //This will also create our players and their data.
+    //Refer to gameplay, which acts as the model
+
+    gameplay.initData(playerData);
+  } //Processing block for submit end.
 })
