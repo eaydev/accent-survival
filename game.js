30b72748f6cac3d923b24a1e55011cad9333ae39
Ephraim Amiel Yusi <ephraimyusi@gmail.com>
2020-01-25 20:28:40 +1000

    CSS fixes and Data is dynamically pulled for intermediary page.

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ let gameplay = {
   //Player Stats Defined.
   players: {
     'Player 1' : {Lives: 3},
-    'Player 2' : {Lives: 3}
+    'Player 2' : {Lives: 1}
 
   },
   //Current Player of the Game.
@@ -15,10 +15,19 @@ let gameplay = {
       this.players['Player ' + (i + 1)] = {"Lives" : arr[1]};
     };
     this.currentPlayer = 'Player 1';
+  },
+  //Method to get the current player's life.
+  getCurrentLife : function(){
+    return this.players[this.currentPlayer].Lives;
   }
 }
 
 function initPlayer(){
-  // document.getElementById('playerDisplay').innerHTML = gameplay.currentPlayer;
-    console.log("Ready");
+    // console.log("Ready");
 }
+
+//Pull data from Object to be displayed in DOM
+(function(){
+  document.getElementById('playerDisplay').innerHTML = gameplay.currentPlayer;
+  document.getElementById('playerLife').innerHTML = gameplay.getCurrentLife();
+})()
