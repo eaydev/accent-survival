49ca683533c95570ab55529f33494f7cc34d32ad
Ephraim Amiel Yusi <ephraimyusi@gmail.com>
2020-01-27 19:25:14 +1000

    Added CSS loader and API request for several different categories

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 // Gameplay object which will manage our data and associate functions with said data
 
 let gameplay = {
+  quote : "",
   //Player Stats Defined.
   players: {
     'Player 1' : {Lives: 3},
@@ -19,31 +20,60 @@ let gameplay = {
   //Method to get the current player's life.
   getCurrentLife : function(){
     return this.players[this.currentPlayer].Lives;
-  }
+  },
+  //Method to get quote and store into game
+  getQuote : function(){
+    const categories = [
+      'inspire',
+      'management',
+      'sports',
+      'life',
+      'funny',
+      'love',
+      'art',
+      'students'];
+    let category = categories[(Math.floor(Math.random() * categories.length))];
+
+    //Create API Request
+    let quoteRequest = new XMLHttpRequest();
+      quoteRequest.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+           let quote = JSON.parse(quoteRequest.responseText);
+           this.quote = (quote['contents']['quotes'][0]['quote']);
+           console.log(this.quote);
+        }
+    };
+    quoteRequest.open("GET", `http://quotes.rest/qod.json?category=${category}`, true);
+    quoteRequest.send();
+      }
 }
 
 let countDown;
 let currentNumber = 3;
-//Upon loading this count-down page we will initiate the animation starter
+
+//Function that facilitates DOM appearance and quote retreival for countdown
 function iterateCount(){
   if(currentNumber === 0){
     stopIterate();
-    console.log('STOPPED!');
     return document.getElementById('App').innerHTML =
-    ``;
+    `<div class="loader"></div>`;
   }
   document.getElementById('App').innerHTML =
   `<h1 id="count" class="black count-header text-white count-anim">${currentNumber}</h1>`;
   currentNumber--;
 }
-
+//Clear Interval for Counter
 function stopIterate(){
   clearInterval(countDown);
 }
 
+//Upon loading this count-down page we will initiate the animation starter
 (function(){
+  gameplay.getQuote();
   document.getElementById('App').innerHTML =
   `<h1 id="count" class="black count-header text-white count-anim">${currentNumber}</h1>`;
   currentNumber--;
+  //Start the counter in the called variable so can call
   countDown = setInterval(iterateCount, 1200);
+
 })();
