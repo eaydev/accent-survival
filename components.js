85a04c3a0bff2188fd511f918870b79da5d0bff2
Ephraim Amiel Yusi <ephraimyusi@gmail.com>
2020-02-01 15:21:37 +1000

    Converted views to ES6 classes for readability and less repeatable code.

--- components.js
+++ components.js
@@ -1,51 +1,62 @@
-const starterScreen = {
-  preRender : function(){
-    return
-      `<!-- Start of hero -->
+function render(screen){
+  document.getElementById("App").innerHTML = screen.preRender;
+  screen.postRender();
+}
 
-      <div class="starter-hero">
-        <img class="brand-logo" src="assets/communication.svg" alt="Accent Survival Logo: An arrow piercing two speech bubbles.">
-        <div class="starter-hero-header black text-white">
-          <h1>Accent Survival</h1>
-        </div>
-        <p class="text-white sub-text">The accent guessing survival game.</p>
-      </div>
+class Screen {
+  constructor(preRender, postRender){
+    this.preRender = preRender;
+    this.postRender = postRender;
+  }
+}
 
-      <!-- end of hero -->
 
-      <!-- start of form -->
+const starterScreen = new Screen (
+  `<!-- Start of hero -->
 
-      <form id="starter-form">
-        <div containerFor="numberOfPlayers" class="form-line button button-orange">
-          <label for="numberOfPlayers">Players: </label>
-          <select name="numberOfPlayers" id="numberOfPlayers" class="border-none">
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
-        </div>
+  <div class="starter-hero">
+    <img class="brand-logo" src="assets/communication.svg" alt="Accent Survival Logo: An arrow piercing two speech bubbles.">
+    <div class="starter-hero-header black text-white">
+      <h1>Accent Survival</h1>
+    </div>
+    <p class="text-white sub-text">The accent guessing survival game.</p>
+  </div>
 
-        <div containerFor="numberOfLives" class="form-line button button-red">
-          <label for="numberOfLives">Lives: </label>
-          <select name="numberOfLives" id="numberOfLives" class="border-none">
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-        </div>
+  <!-- end of hero -->
 
-        <button class="button button-large button-green">Start</button>
-      </form>
+  <!-- start of form -->
 
-      <!-- end of form -->`
-  },
-  postRender : function(){
+  <form id="starter-form">
+    <div containerFor="numberOfPlayers" class="form-line button button-orange">
+      <label for="numberOfPlayers">Players: </label>
+      <select name="numberOfPlayers" id="numberOfPlayers" class="border-none">
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+    </div>
+
+    <div containerFor="numberOfLives" class="form-line button button-red">
+      <label for="numberOfLives">Lives: </label>
+      <select name="numberOfLives" id="numberOfLives" class="border-none">
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+    </div>
+
+    <button class="button button-large button-green">Start</button>
+  </form>
+
+  <!-- end of form -->`,
+
+  function(){
     // function for submitting of start form
     document.getElementById("starter-form").addEventListener('click', function(e){
       //Start of submit processing block
@@ -65,33 +76,28 @@ const starterScreen = {
       } //Processing block for submit end.
     })
   }
-}
+);
 
+const readyScreen = new Screen(
+  `<div class="intermediary-header">
+    <h1 id="playerDisplay" class="starter-hero-header text-white black" style="padding-bottom: 20px; letter-spacing: 0.035em;">
+      Player 1
+    </h1>
+    <p class="semi-bold text-white underline">Lives: <span id="playerLife">2</span></p>
+  </div>
+  <button class="button button-large button-orange">Let's go</button>`,
 
-const readyScreen = {
-  preRender : function(){
-    return
-    `<div class="intermediary-header">
-      <h1 id="playerDisplay" class="starter-hero-header text-white black" style="padding-bottom: 20px; letter-spacing: 0.035em;">
-        Player 1
-      </h1>
-      <p class="semi-bold text-white underline">Lives: <span id="playerLife">2</span></p>
-    </div>
-    <button class="button button-large button-orange">Let's go</button>`;
-  },
-  postRender : function(){
+  function(){
     //Pull data from Object to be displayed in DOM
       document.getElementById('playerDisplay').innerHTML = gameplay.currentPlayer;
       document.getElementById('playerLife').innerHTML = gameplay.getCurrentLife();
   }
-}
+)
 
-const countDownScreen = {
-  preRender : function(){
-    return
-      `    <h1 id="count" class="black count-header text-white count-anim">3</h1>`
-  },
-  postRender : function(){
+const countDownScreen = new Screen(
+  `<h1 id="count" class="black count-header text-white count-anim">3</h1>`,
+
+  function(){
     let countDown;
     let currentNumber = 3;
 
@@ -122,38 +128,34 @@ const countDownScreen = {
 
     })();
   }
-}
+);
 
-const quoteScreen = {
-  preRender : function(){
-    const template =
-    `<h1 id="playerID" class="semi-bold text-white" style="letter-spacing: 0.035em;">Player 2</h1>
-    <!-- Quote Card container -->
-    <div class="quote-card-container">
-      <div class="quote-card">
-        <h1 id="quote">
-          <div class="loader"></div>
-        </h1>
-      </div>
-      <span style="margin-top: 5px; z-index:50;font-size:0.9em; opacity: 0.7; transform: scale(0.8)"><img src="https://theysaidso.com/branding/theysaidso.png" height="20" width="20" alt="theysaidso.com"/><a href="https://theysaidso.com" title="Powered by quotes from theysaidso.com" style="color: #9fcc25; margin-left: 4px; vertical-align: middle;">theysaidso.com</a></span>
 
-      <div class="accent-container text-white" style="margin-top: 20px;">
-        <h2 style="text-decoration:underline" class="semi-bold">Accent</h2>
-        <h1 id="accent" class="black starter-hero-header">Picking...</h1>
-      </div>
+const quoteScreen = new Screen(
+  `<h1 id="playerID" class="semi-bold text-white" style="letter-spacing: 0.035em;">Player 2</h1>
+  <!-- Quote Card container -->
+  <div class="quote-card-container">
+    <div class="quote-card">
+      <h1 id="quote">
+        <div class="loader"></div>
+      </h1>
     </div>
-    <!-- Accent container -->
+    <span style="margin-top: 5px; z-index:50;font-size:0.9em; opacity: 0.7; transform: scale(0.8)"><img src="https://theysaidso.com/branding/theysaidso.png" height="20" width="20" alt="theysaidso.com"/><a href="https://theysaidso.com" title="Powered by quotes from theysaidso.com" style="color: #9fcc25; margin-left: 4px; vertical-align: middle;">theysaidso.com</a></span>
+
+    <div class="accent-container text-white" style="margin-top: 20px;">
+      <h2 style="text-decoration:underline" class="semi-bold">Accent</h2>
+      <h1 id="accent" class="black starter-hero-header">Picking...</h1>
+    </div>
+  </div>
+  <!-- Accent container -->
 
 
-    <div class="">
-      <div class="text-white">TIME(s): <span id="timer">30</span></div>
-      <button class="button button-red button-large text-white">GUESSED!</button>
-    </div>`;
+  <div class="">
+    <div class="text-white">TIME(s): <span id="timer">30</span></div>
+    <button class="button button-red button-large text-white">GUESSED!</button>
+  </div>`,
 
-    return template;
-    ;
-  },
-  postRender : function(){
+  function(){
       //Pasting Values into DOM
     document.getElementById("playerID").innerHTML = gameplay.currentPlayer;
       //Controlling style for quote card regarding character length.
@@ -167,7 +169,7 @@ const quoteScreen = {
 
       //Timer module
     let time = 5;
-    
+
     function timer(){
       if (time === 0) {
         console.log("OUT OF TIME NIGGUH!");
@@ -180,4 +182,4 @@ const quoteScreen = {
     timer();
     let countDown = setInterval(timer, 1000);
   }
-}
+);
