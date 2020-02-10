7f8733b78f846de0b19fad6867b81d038904ac64
Ephraim Amiel Yusi <ephraimyusi@gmail.com>
2020-02-10 22:43:32 +1000

    Player count screen pending, hence currently in view.

--- game.js
+++ game.js
@@ -1,18 +1,17 @@
 // Gameplay object which will manage our data and associate functions with said data
 
 let gameplay = {
-  view: "",
+  view: "starterScreen",
   // State of guess
   guessed : false,
   //Quote retreived from API
   quote : "",
   //Player Stats Defined.
-  players: {"Player 1": {Lives: 3},
-            "Player 2": {Lives : 0}
-
+  players: {"Player 1": {Lives: 1},
+            "Player 2": {Lives : 3}
            },
   //Current Player of the Game.
-  currentPlayer : '',
+  currentPlayer : "Player 1",
   // Method to initialise the player and player data
   createPlayers: function(arr){
     for (let i = 0; i < arr[0]; i++) {
